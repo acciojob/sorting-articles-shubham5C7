f95057db83a13801b99ec6a5ed89d266514d1e6d
shubham somavamshi <shubhamraopatil123@gmail.com>
2025-09-15 22:19:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+const bands = [
+      'The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil',
+      'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State',
+      'We Came as Romans', 'Counterparts', 'Oh, Sleeper',
+      'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'
+    ];
+
+function clearName(name){
+  return name.replace(/^(a |an |the )/i, "").toLowerCase();
+}
+
+  bands.sort((a, b) => clearName(a).localeCompare(clearName(b)));
+
+  const ul = document.getElementById("band");
+ for(let band of bands){
+  const li = document.createElement("li");
+  li.textContent=band;
+  ul.appendChild(li);
+ }
+
